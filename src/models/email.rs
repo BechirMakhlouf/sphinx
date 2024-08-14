72853a342dd11dd36482f662c28d42291c04035b
bechirMakhlouf <bechirM@proton.me>
2024-08-14 20:38:06 +0100

    change parse to tryFrom

--- src/models/email.rs
+++ src/models/email.rs
@@ -1,6 +1,7 @@
 #[derive(Debug, serde::Deserialize, Clone)]
 pub struct Email(String);
 
+//TODO: make proper error handling with thiserror
 impl TryFrom<&str> for Email {
     type Error = String;
     fn try_from(email: &str) -> Result<Self, Self::Error> {
@@ -11,18 +12,11 @@ impl TryFrom<&str> for Email {
         }
     }
 }
+
 impl Email {
-    pub fn parse(email: &str) -> Result<Self, &str> {
-        let is_valid = validator::ValidateEmail::validate_email(&email);
-        match is_valid {
-            true => Ok(Self(email.into())),
-            false => Err("Invalid Email"),
-        }
-    }
     pub fn from_trusted_str(email: &str) -> Self {
         Self(email.to_string())
     }
-
     pub fn as_str(&self) -> &str {
         self.0.as_str()
     }
