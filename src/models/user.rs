0f5e830de7e703922338a0ada26674ff82f2f9c9
bechirMakhlouf <bechirM@proton.me>
2024-09-14 20:51:34 +0100

    add user builder

--- src/models/user.rs
+++ src/models/user.rs
@@ -75,4 +75,98 @@ impl User {
             updated_at: Utc::now(),
         }
     }
+    pub fn builder(email: Email) -> UserBuilder {
+        UserBuilder::new(email)
+    }
+}
+
+#[derive(Debug)]
+pub struct UserBuilder {
+    pub id: Id,
+    pub email: Email,
+    pub email_confirmed_at: Option<DateTime<Utc>>,
+
+    pub phone: Option<String>,
+    pub phone_confirmed_at: Option<DateTime<Utc>>,
+
+    pub encrypted_password: Option<EncryptedPassword>,
+
+    pub last_sign_in_at: Option<DateTime<Utc>>,
+    pub is_admin: bool,
+
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+}
+
+impl UserBuilder {
+    pub fn new(email: Email) -> Self {
+        Self {
+            id: Id::new(),
+            email,
+            encrypted_password: None,
+            email_confirmed_at: None,
+            phone: None,
+            phone_confirmed_at: None,
+            last_sign_in_at: None,
+            is_admin: false,
+            created_at: Utc::now(),
+            updated_at: Utc::now(),
+        }
+    }
+
+    pub fn email_confirmed_at(mut self, confirmed_at: Option<DateTime<Utc>>) -> Self {
+        self.email_confirmed_at = confirmed_at;
+        self
+    }
+
+    pub fn phone_confirmed_at(mut self, confirmed_at: Option<DateTime<Utc>>) -> Self {
+        self.phone_confirmed_at = confirmed_at;
+        self
+    }
+    // Set phone and phone_confirmed_at fields
+
+    pub fn phone(mut self, phone: String) -> Self {
+        self.phone = Some(phone);
+        self
+    }
+
+    // Set encrypted_password field
+    pub fn encrypted_password(mut self, encrypted_password: EncryptedPassword) -> Self {
+        self.encrypted_password = Some(encrypted_password);
+        self
+    }
+
+    // Set last_sign_in_at field
+    pub fn last_sign_in_at(mut self, last_sign_in_at: DateTime<Utc>) -> Self {
+        self.last_sign_in_at = Some(last_sign_in_at);
+        self
+    }
+
+    // Set is_admin field
+    pub fn is_admin(mut self, is_admin: bool) -> Self {
+        self.is_admin = is_admin;
+        self
+    }
+
+    // Set updated_at field
+    pub fn updated_at(mut self, updated_at: DateTime<Utc>) -> Self {
+        self.updated_at = updated_at;
+        self
+    }
+
+    // Finalize and build the User object
+    pub fn build(self) -> User {
+        User {
+            id: self.id,
+            email: self.email,
+            email_confirmed_at: self.email_confirmed_at,
+            phone: self.phone,
+            phone_confirmed_at: self.phone_confirmed_at,
+            encrypted_password: self.encrypted_password,
+            last_sign_in_at: self.last_sign_in_at,
+            is_admin: self.is_admin,
+            created_at: self.created_at,
+            updated_at: self.updated_at,
+        }
+    }
 }
