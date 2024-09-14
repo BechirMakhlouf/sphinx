5586a70840ec32cd20814259e7c637c7218875e5
bechirMakhlouf <bechirM@proton.me>
2024-09-14 20:48:47 +0100

    add oauth settings

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,6 @@ pub mod oauth;
 pub mod password;
 pub mod repositories;
 pub mod routes;
-pub mod services;
 
 pub fn init_tracing() {
     use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
@@ -34,16 +33,15 @@ pub async fn configure_app(
     let repository = repositories::Repository::new(&config.database, &config.redis, &config.jwt);
     let token_factory = models::token::TokenFactory::new(config.jwt);
     let mailer = mailer::Mailer::new(config.smtp);
-
     let authenticator = std::sync::Arc::new(authenticator::Authenticator::new(
         repository,
         token_factory,
         mailer,
         config.application.config,
+        config.oauth,
     ));
 
     let app = routes::get_router(authenticator);
-
     let listener = tokio::net::TcpListener::bind(format!(
         "{}:{}",
         config.application.host, config.application.port
@@ -52,6 +50,7 @@ pub async fn configure_app(
     .expect("failed to bind to port");
 
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
+
     axum::serve(
         listener,
         app.into_make_service_with_connect_info::<std::net::SocketAddr>(),
