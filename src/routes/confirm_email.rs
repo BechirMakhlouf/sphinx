a585361fe74a681390343a8f8291d3284d007968
bechirMakhlouf <bechirM@proton.me>
2024-09-14 20:54:35 +0100

    make confirm email redirect to callback

--- src/routes/confirm_email.rs
+++ src/routes/confirm_email.rs
@@ -16,11 +16,10 @@ pub async fn confirm_email(
     params: Query<Params>,
 ) -> impl IntoResponse {
     match authenticator.confirm_email(&params.token).await {
-        Ok(_) => "email confirmed".to_string().into_response(),
+        Ok(_) => Redirect::to(authenticator.get_confirm_email_callback().as_ref()).into_response(),
         Err(authenticator::Error::InvalidToken) => {
             (StatusCode::BAD_REQUEST, "invalid token").into_response()
         }
         Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Internal Error").into_response(),
-    };
-    Redirect::to("https://www.google.com")
+    }
 }
